74f5614232a5269b0642dc88b170cf4d17461fa9
Chris Dempewolf <cdempewolf@simplymeasured.com>
2015-12-08 11:59:10 -0600

    Rename PinResponse -> CompletePinResponse

--- src/test/java/responses/TestPins.java
+++ src/test/java/responses/TestPins.java
@@ -21,7 +21,7 @@ public class TestPins {
     //API call: https://api.pinterest.com/v1/pins/525091637782793357/?access_token=ARp8HNzfw2scBtJqtYA57a8xFeAqFBzML4SnnEZCe9w6DeAJGAAAAAA&fields=id,link,counts,note,id,link,creator%28id,first_name,last_name,url%29,board%28id,name,url%29
     @Test
     public void testPinResponse() throws IOException {
-        final String response = loadFile("responses/PinResponse.json");
+        final String response = loadFile("responses/CompletePinResponse.json");
         final Pin pin = new Gson().fromJson(response, Pin.class);
 
         System.out.println(pin);
