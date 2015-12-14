02521f4d9b516f139e7e430613f8306f1754d105
Chris Dempewolf <cdempewolf@simplymeasured.com>
2015-12-14 14:16:57 -0600

    Don't convert to String in end point builder unit test

--- src/test/java/com/chrisdempewolf/pinterest/TestEndPointURIBuilder.java
+++ src/test/java/com/chrisdempewolf/pinterest/TestEndPointURIBuilder.java
@@ -8,11 +8,7 @@ import java.net.URISyntaxException;
 import static com.chrisdempewolf.pinterest.EndPointURIBuilder.*;
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by evan on 12/14/15.
- */
 public class TestEndPointURIBuilder {
-
     final String accessToken = "testaccesstoken123";
     final String id = "1234567890";
     final String fields = "myFields";
@@ -23,41 +19,41 @@ public class TestEndPointURIBuilder {
     @Test
     public void testBuildPinUri() throws URISyntaxException {
         final URI actual = buildPinUri(accessToken, id, fields);
-        final String expected = "https://api.pinterest.com/v1/pins/" + id + "/?access_token=" + accessToken + "&fields=" + fields;
+        final URI expected = new URI("https://api.pinterest.com/v1/pins/" + id + "/?access_token=" + accessToken + "&fields=" + fields);
 
-        assertEquals(expected, actual.toString());
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testBuildMyPinUri() throws URISyntaxException {
         final URI actual = buildMyPinUri(accessToken, fields);
-        final String expected = "https://api.pinterest.com/v1/me/pins/?access_token=" + accessToken + "&fields=" + fields;
+        final URI expected = new  URI("https://api.pinterest.com/v1/me/pins/?access_token=" + accessToken + "&fields=" + fields);
 
-        assertEquals(expected, actual.toString());
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testBuildBoardPinUri() throws URISyntaxException {
         final URI actual = buildBoardPinUri(accessToken, name, fields);
-        final String expected = "https://api.pinterest.com/v1/boards/" + name + "/pins/?access_token=" + accessToken + "&fields=" + fields;
+        final URI expected = new  URI("https://api.pinterest.com/v1/boards/" + name + "/pins/?access_token=" + accessToken + "&fields=" + fields);
 
-        assertEquals(expected, actual.toString());
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testBuildBoardUri() throws URISyntaxException {
         final URI actual = buildBoardUri(accessToken, userName, boardName, fields);
-        final String expected = "https://api.pinterest.com/v1/boards/" + userName + "/" + boardName + "/?access_token=" + accessToken + "&fields=" + fields;
+        final URI expected = new  URI("https://api.pinterest.com/v1/boards/" + userName + "/" + boardName + "/?access_token=" + accessToken + "&fields=" + fields);
 
-        assertEquals(expected, actual.toString());
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testBuildMyBoardUri() throws URISyntaxException {
         final URI actual = buildMyBoardUri(accessToken, fields);
-        final String expected = "https://api.pinterest.com/v1/me/boards/?access_token=" + accessToken + "&fields=" + fields;
+        final URI expected = new  URI("https://api.pinterest.com/v1/me/boards/?access_token=" + accessToken + "&fields=" + fields);
 
-        assertEquals(expected, actual.toString());
+        assertEquals(expected, actual);
     }
 
 }
