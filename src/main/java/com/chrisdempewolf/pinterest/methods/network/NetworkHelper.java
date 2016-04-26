d626c13adc96625305049d5ea4057375ef12923b
Chris Dempewolf <cdempewolf@simplymeasured.com>
2016-04-26 15:33:41 -0700

    Update network helper to include POST requests

--- src/main/java/com/chrisdempewolf/pinterest/methods/network/NetworkHelper.java
+++ src/main/java/com/chrisdempewolf/pinterest/methods/network/NetworkHelper.java
@@ -2,6 +2,8 @@ package com.chrisdempewolf.pinterest.methods.network;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
@@ -10,7 +12,15 @@ import java.io.IOException;
 public class NetworkHelper {
 
     public static ResponseMessageAndStatusCode submitDeleteRequest(final HttpDelete httpDelete) throws IOException {
-        try (final CloseableHttpResponse response = HttpClients.createDefault().execute(httpDelete)) {
+        return submitRequest(httpDelete);
+    }
+
+    public static ResponseMessageAndStatusCode submitPostRequest(final HttpPost httpPost) throws IOException {
+        return submitRequest(httpPost);
+    }
+
+    private static ResponseMessageAndStatusCode submitRequest(final HttpRequestBase httpRequestBase) throws IOException {
+        try (final CloseableHttpResponse response = HttpClients.createDefault().execute(httpRequestBase)) {
             final String responseBody = EntityUtils.toString(response.getEntity());
             final int statusCode = response.getStatusLine().getStatusCode();
 
