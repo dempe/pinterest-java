6c7fc978846b4ed0a901c6f31125aef166956132
Chris Dempewolf <cdempewolf@simplymeasured.com>
2017-02-16 01:01:10 -0800

    update network helper to use http client fluid interface

--- src/main/java/com/chrisdempewolf/pinterest/methods/network/NetworkHelper.kt
+++ src/main/java/com/chrisdempewolf/pinterest/methods/network/NetworkHelper.kt
@@ -1,5 +1,8 @@
 package com.chrisdempewolf.pinterest.methods.network
 
+import org.apache.http.HttpResponse
+import org.apache.http.NameValuePair
+import org.apache.http.client.fluent.Request
 import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.client.methods.HttpDelete
 import org.apache.http.client.methods.HttpPost
@@ -8,12 +11,18 @@ import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
 
 import java.io.IOException
+import java.net.URI
 
 object NetworkHelper {
 
+    @JvmStatic
     @Throws(IOException::class)
-    fun submitDeleteRequest(httpDelete: HttpDelete): ResponseMessageAndStatusCode {
-        return submitRequest(httpDelete)
+    fun submitDeleteRequest(url: URI): ResponseMessageAndStatusCode {
+        val response = Request.Delete(url)
+                .execute()
+                .returnResponse()
+
+        return convertResponse(response)
     }
 
     @Throws(IOException::class)
@@ -21,11 +30,10 @@ object NetworkHelper {
         return submitRequest(httpPost)
     }
 
-    @Throws(IOException::class)
-    private fun submitRequest(httpRequestBase: HttpRequestBase): ResponseMessageAndStatusCode {
-        HttpClients.createDefault().execute(httpRequestBase).use { response ->
-            val responseBody = EntityUtils.toString(response.entity)
-            val statusCode = response.statusLine.statusCode
+    @JvmStatic
+    private fun convertResponse(response: HttpResponse): ResponseMessageAndStatusCode {
+        return ResponseMessageAndStatusCode(response.statusLine.statusCode, EntityUtils.toString(response.entity))
+    }
 
             return ResponseMessageAndStatusCode(statusCode, responseBody)
         }
