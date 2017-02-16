c2ecbab0780c46e373ac2ee5465b27a2e3d6920e
Chris Dempewolf <cdempewolf@simplymeasured.com>
2017-02-16 01:02:42 -0800

    remove companion object from end point builder

--- src/main/java/com/chrisdempewolf/pinterest/methods/pin/PinEndPointURIBuilder.kt
+++ src/main/java/com/chrisdempewolf/pinterest/methods/pin/PinEndPointURIBuilder.kt
@@ -8,61 +8,60 @@ import java.util.regex.Pattern
 
 import org.apache.commons.lang3.StringUtils.isNotBlank
 
-class PinEndPointURIBuilder private constructor() {
+object PinEndPointURIBuilder {
+    private const val BASE_URL = "https://api.pinterest.com"
+    private const val BOARD_PIN_PATH = "/v1/boards/{BOARD_NAME}/pins/"
+    private const val BASE_PIN_PATH = "/v1/pins"
+    private const val PIN_PATH = BASE_PIN_PATH + "/{PIN_ID}"
+    private const val MY_PIN_PATH = "/v1/me/pins/"
+    private val PIN_PATTERN = Pattern.compile("\\{PIN_ID\\}")
+    private val BOARD_NAME_PATTERN = Pattern.compile("\\{BOARD_NAME\\}")
 
-    init {
-        throw IllegalStateException("Instantiation not allowed")
+    init { throw IllegalStateException("Instantiation not allowed") }
+
+    @JvmStatic
+    @Throws(URISyntaxException::class)
+    fun buildBasePinUri(accessToken: String): URI {
+        return URIBuilder(BASE_URL).setPath(BASE_PIN_PATH).setParameter("access_token", accessToken).build()
     }
 
-    companion object {
-        private val BASE_URL = "https://api.pinterest.com"
-        private val BOARD_PIN_PATH = "/v1/boards/{BOARD_NAME}/pins/"
-        private val BASE_PIN_PATH = "/v1/pins"
-        private val PIN_PATH = BASE_PIN_PATH + "/{PIN_ID}"
-        private val MY_PIN_PATH = "/v1/me/pins/"
-        private val PIN_PATTERN = Pattern.compile("\\{PIN_ID\\}")
-        private val BOARD_NAME_PATTERN = Pattern.compile("\\{BOARD_NAME\\}")
+    @JvmStatic
+    @Throws(URISyntaxException::class)
+    fun buildPinUri(accessToken: String, id: String, fields: String?): URI {
+        val uriBuilder = URIBuilder(BASE_URL)
+                .setPath(PIN_PATTERN.matcher(PIN_PATH).replaceFirst(id))
+                .setParameter("access_token", accessToken)
 
-        @Throws(URISyntaxException::class)
-        fun buildBasePinUri(accessToken: String): URI {
-            return URIBuilder(BASE_URL).setPath(BASE_PIN_PATH).setParameter("access_token", accessToken).build()
+        if (isNotBlank(fields)) {
+            uriBuilder.setParameter("fields", fields)
         }
 
-        @Throws(URISyntaxException::class)
-        fun buildPinUri(accessToken: String, id: String, fields: String?): URI {
-            val uriBuilder = URIBuilder(BASE_URL)
-                    .setPath(PIN_PATTERN.matcher(PIN_PATH).replaceFirst(id))
-                    .setParameter("access_token", accessToken)
+        return uriBuilder.build()
+    }
 
-            if (isNotBlank(fields)) {
-                uriBuilder.setParameter("fields", fields)
-            }
+    @JvmStatic
+    @Throws(URISyntaxException::class)
+    fun buildMyPinUri(accessToken: String, fields: String?): URI {
+        val uriBuilder = URIBuilder(BASE_URL).setPath(MY_PIN_PATH).setParameter("access_token", accessToken)
 
-            return uriBuilder.build()
+        if (isNotBlank(fields)) {
+            uriBuilder.setParameter("fields", fields)
         }
 
-        @Throws(URISyntaxException::class)
-        fun buildMyPinUri(accessToken: String, fields: String?): URI {
-            val uriBuilder = URIBuilder(BASE_URL).setPath(MY_PIN_PATH).setParameter("access_token", accessToken)
+        return uriBuilder.build()
+    }
 
-            if (isNotBlank(fields)) {
-                uriBuilder.setParameter("fields", fields)
-            }
+    @JvmStatic
+    @Throws(URISyntaxException::class)
+    fun buildBoardPinUri(accessToken: String, name: String, fields: String?): URI {
+        val uriBuilder = URIBuilder(BASE_URL)
+                .setPath(BOARD_NAME_PATTERN.matcher(BOARD_PIN_PATH).replaceFirst(name))
+                .setParameter("access_token", accessToken)
 
-            return uriBuilder.build()
+        if (isNotBlank(fields)) {
+            uriBuilder.setParameter("fields", fields)
         }
 
-        @Throws(URISyntaxException::class)
-        fun buildBoardPinUri(accessToken: String, name: String, fields: String?): URI {
-            val uriBuilder = URIBuilder(BASE_URL)
-                    .setPath(BOARD_NAME_PATTERN.matcher(BOARD_PIN_PATH).replaceFirst(name))
-                    .setParameter("access_token", accessToken)
-
-            if (isNotBlank(fields)) {
-                uriBuilder.setParameter("fields", fields)
-            }
-
-            return uriBuilder.build()
-        }
+        return uriBuilder.build()
     }
 }
--- src/test/java/com/chrisdempewolf/pinterest/methods/pin/TestPinEndPointURIBuilder.java
+++ src/test/java/com/chrisdempewolf/pinterest/methods/pin/TestPinEndPointURIBuilder.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import static com.chrisdempewolf.pinterest.methods.pin.PinEndPointURIBuilder.Companion;
 import static org.junit.Assert.assertEquals;
 
 public class TestPinEndPointURIBuilder {
@@ -16,21 +15,21 @@ public class TestPinEndPointURIBuilder {
 
     @Test
     public void testBuildPinUri() throws URISyntaxException {
-        final URI actualURI = Companion.buildPinUri(ACCESS_TOKEN, PIN_ID, FIELDS);
+        final URI actualURI = PinEndPointURIBuilder.buildPinUri(ACCESS_TOKEN, PIN_ID, FIELDS);
         final URI expectedURI = new URI("https://api.pinterest.com/v1/pins/" + PIN_ID + "?access_token=" + ACCESS_TOKEN + "&fields=" + FIELDS);
         assertEquals(expectedURI, actualURI);
     }
 
     @Test
     public void testBuildMyPinUri() throws URISyntaxException {
-        final URI actualURI = Companion.buildMyPinUri(ACCESS_TOKEN, FIELDS);
+        final URI actualURI = PinEndPointURIBuilder.buildMyPinUri(ACCESS_TOKEN, FIELDS);
         final URI expectedURI = new  URI("https://api.pinterest.com/v1/me/pins/?access_token=" + ACCESS_TOKEN + "&fields=" + FIELDS);
         assertEquals(expectedURI, actualURI);
     }
 
     @Test
     public void testBuildBoardPinUri() throws URISyntaxException {
-        final URI actualURI = Companion.buildBoardPinUri(ACCESS_TOKEN, USER_NAME, FIELDS);
+        final URI actualURI = PinEndPointURIBuilder.buildBoardPinUri(ACCESS_TOKEN, USER_NAME, FIELDS);
         final URI expectedURI = new  URI("https://api.pinterest.com/v1/boards/" + USER_NAME + "/pins/?access_token=" + ACCESS_TOKEN + "&fields=" + FIELDS);
         assertEquals(expectedURI, actualURI);
     }
