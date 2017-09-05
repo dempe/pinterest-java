7fa236ef4be4cfe8ca1e2068ecfe570fdee84b89
Chris Dempewolf <cdempewolf@simplymeasured.com>
2017-09-05 15:04:21 -0700

    add Board counts to response

--- src/main/java/com/chrisdempewolf/pinterest/responses/pin/Board.kt
+++ src/main/java/com/chrisdempewolf/pinterest/responses/pin/Board.kt
@@ -1,3 +1,5 @@
 package com.chrisdempewolf.pinterest.responses.pin
 
-data class Board(val id: String?, val name: String?, val url: String?)
+import com.google.gson.annotations.SerializedName
+
+data class Board(val id: String?, val name: String?, val url: String?, @SerializedName("counts") val boardCounts: BoardCounts?)
---
+++ src/main/java/com/chrisdempewolf/pinterest/responses/pin/BoardCounts.kt
@@ -0,0 +1,3 @@
+package com.chrisdempewolf.pinterest.responses.pin
+
+data class BoardCounts(val pins: Int?, val collaborators: Int?, val followers: Int?)
